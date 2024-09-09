d89a3a0d6d7f4aa9979c72cd2b1db2b16ed98a85
UMAIR AHMED <umairahmed51u@gmail.com>
2024-09-09 03:44:30 -0700

    Add files via upload

---
+++ Jawa-script-1/sendbox.js
@@ -0,0 +1,36 @@
+// task-1
+alert("Hello")
+// task-2
+alert("Error! please enter a walid password")
+// task-3
+alert("Welcome to Js land\n Happy Coding!")
+// task-4
+alert("Welcome to js Land")
+// task-5
+alert("Hello....I can run Js through my web browsers cansole")
+
+// Assignment-2
+// task-1
+var username = ("")
+// task-2
+var myname =("Umair Ahmed")
+console.log(myname);
+// task-3
+var message =("hello world")
+alert("hello world")
+// task-4
+var name =("jhone Done")
+alert("jhone Done") 
+var  Age =("15 Years Old")
+alert("15 Years Old")
+var data =("certified Application Development")
+alert("certified Application Development")
+// task-5
+var pizza =("pizza")
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// task-6
+var Email =("My email address is example@example.com")
+alert("My email address is example@example.com")
+// task-7
+var book =("I am trying to learn from the book A smarter way to learn Jawascript")
+alert("I am trying to learn from the book A smarter way to learn Jawascript")
