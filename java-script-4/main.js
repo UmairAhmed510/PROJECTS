ba17bac7ed5e9aafc6c83512fffc833b183c85b2
UMAIR AHMED <umairahmed51u@gmail.com>
2024-10-14 04:40:35 -0700

    Add files via upload

---
+++ java-script-4/main.js
@@ -0,0 +1,27 @@
+let heading= document.querySelector(".heading-1")
+let input= document.querySelector(".input")
+
+let button= document.querySelector(".list-1")
+button.addEventListener("click", function(){
+    heading.textContent=input.value
+})
+let headings =document.querySelector(".heading-2")
+let style =document.querySelector(".input")
+
+let description= document.querySelector(".list-2")
+description.addEventListener("click", function(){
+    headings.textContent=input.value
+})  
+
+let box= document.querySelector("list-3")
+box.addEventListener("click",function(){})
+
+let mobile= document.querySelector(".list-4")
+mobile.addEventListener("click",function(){
+    
+})
+
+let laptop= document.querySelector("list-5")
+laptop.addEventListener("click",function(){})
+
+
